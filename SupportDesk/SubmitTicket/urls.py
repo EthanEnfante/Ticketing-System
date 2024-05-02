267fe7a435c920ae70911b8748042c55c8f8552c
Ethan Enfante <fake_email@gmail.com>
2024-05-01 18:16:18 -0700

    DELETE ALL FILES PRETAINING TO DJANGO PROJECT

--- SupportDesk/SubmitTicket/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'SubmitTicket'
-urlpatterns = [
-    path("", views.index, name='index')
-]
--- SupportDesk/SubmitTicket/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def index(request):
-    context = 'Hello World!'
-    return render(request, template_name='submission_form/index.html', context=context)
